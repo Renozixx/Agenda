826ee0ed9da713a8cede9b683824750157facdb7
Hugo Otávio dos Santos de Paula <hugotavio2005@gmail.com>
2024-09-25 09:20:31 -0300

    Renomeado 'ValidatorController.php'; Refatorado 'ValidatorController.php'
    Eu renomeei o controlador 'ValidatorController' para 'ValidadorController', apenas uma correção de nomes.
    
    Refatorei o 'ValidatorController', dividindo as etapas existentes no método 'validade' em outros métodos.

---
+++ app/Controllers/ValidadorController.php
@@ -0,0 +1,119 @@
+<?php
+namespace app\Controllers;
+
+use Exception;
+
+require_once "./autoloader.php";
+
+class ValidatorController {
+    private $msg = [];
+    private $error;
+
+    public function validate ($valor)
+    {
+        foreach ($valor as $value => $rule)
+        {
+            if (!is_array($value)) // Verifica se o valor a ser validado não é um array
+            {
+                $this->processRule($value, $rule);
+            }else
+            {
+                $this->msg[] = "Nenhum valor deve ser um array.";
+                $this->error = TRUE;
+            }
+        }
+        if ($this->error) $this->throwError();
+    }
+
+    private function throwError () // Trata os erros de forma simples
+    {
+        $msg = "";
+        foreach ($this->msg as $v)
+        {
+            $msg .= $v.". ";
+        }
+        throw new Exception("$msg"); // Joga uma excesão
+    }
+
+    private function processRule ($valor, $rule)
+    {
+        if (strstr($rule, ",")) // Verifica se existe mais de 1 regra de validação
+        {
+            $rules = explode(",", $rule);
+
+            foreach ($rules as $kr => $vr)
+            {
+                $this->handleRule($valor, $vr);
+            }
+        }else // Ou se há apena 1 regra de validação
+        {
+            $this->handleRule($valor, $rule);
+        }
+    }
+
+    private function handleRule ($valor, $rule)
+    {
+        if (strstr($rule, ":")) // Verifica se a regra possui algum valor
+        {
+            $rule = explode(":", $rule);
+            $validador = $rule[0];
+            $validadorValor = $rule[1];
+            $this->switchRules($valor, $validador, $validadorValor);
+        }else
+        {
+            $this->switchRules($valor, $rule);
+        }
+    }
+
+    private function switchRules ($valor, $validador, $valueValidador="") // Método para exibir e tratar cada regra de validação. Aqui podemos criar diversas regras
+    {
+        switch ($validador)
+        {
+            case "min":
+                if (strlen($valor) < intval($valueValidador))
+                {
+                    $this->msg[] = "O campo $valor suporta no minímo $valueValidador caracteres";
+                    $this->error = TRUE;
+                }
+                break;
+            case "max":
+                if (strlen($valor) > intval($valueValidador))
+                {
+                    $this->msg[] = "O campo $valor suporta no máximo $valueValidador caracteres";
+                    $this->error = TRUE;
+                }
+                break;
+            case "str":
+                if (!is_string($valor))
+                {
+                    $this->msg[] = "O campo $valor tem que ser uma string";
+                    $this->error = TRUE;
+                }
+                break;
+            case "int":
+                if (!is_int($valor))
+                {
+                    $this->msg[] = "O campo $valor tem que ser um número";
+                    $this->error = TRUE;
+                }
+                break;
+            case "mail":
+                if (!strstr($valor, "@"))
+                {
+                    $this->msg[] = "O campo $valor não é um email válido";
+                    $this->error = TRUE;
+                }
+                break;
+            case "required":
+                if (strlen($valor) == 0)
+                {
+                    $this->msg[] = "O campo $valor não é obrigatório";
+                    $this->error = TRUE;
+                }
+                break;
+            default:
+
+        }
+        return TRUE;
+    }
+}
