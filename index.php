83d15d0e9df5d0b03c163d8fa0147d5c72d2bb24
Hugo Otávio dos Santos de Paula <hugotavio2005@gmail.com>
2024-09-27 09:31:18 -0300

    gitignore; index.php; install tailwindcss; css; headers

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
 <?php
 require_once "./autoloader.php";
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="./public/css/index.css">
+</head>
+<?php require_once "./resources/views/components/headers/header.php"; ?>
+<body class="bg-slate-950">
+</body>
+</html>
---
+++ resources/views/components/headers/header.php
@@ -0,0 +1,6 @@
+<header class="flex justify-center p-3">
+    <ul class="flex gap-2">
+        <li><a href="" class="underline">Home</a></li>
+        <li><a href="" class="underline">Login</a></li>
+    </ul>
+</header>
