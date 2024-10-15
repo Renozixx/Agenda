e1c7b634aadd00871d9c2be133347325463b9a70
Hugo Otávio dos Santos de Paula <hugotavio2005@gmail.com>
2024-10-15 11:02:46 -0300

    TaskAsideController

--- app/Controllers/TaskAsideController.php
+++ app/Controllers/TaskAsideController.php
@@ -1,7 +1,22 @@
 <?php
 
 namespace app\Controllers;
+require_once "./autoloader.php";
 
-class TaskAsideController {
-    
+use src\Request;
+class TaskAsideController extends Request {
+    public function getDatasUrl ()
+    {
+        $url = $this->getURL();
+        if (strstr($url, ".php"))
+        {
+            $date = explode(".php/", $url)[1];
+            $date = explode("/", $date);
+            return [
+                "day" => $date[2],
+                "month" => $date[1],
+                "year" => $date[0],
+            ];
+        }
+    }
 }
